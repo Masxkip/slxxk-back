0a54db447c9abbd36b620e534a7f93efe3c8688f
masxkip <jesufemiketiku@gmail.com>
2025-03-20 03:05:10 -0700

    Your commit message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const User = require("./models/user");
+const User = require("./models/User");
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
