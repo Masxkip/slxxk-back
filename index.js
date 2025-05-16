21e4a90cd309e82dd4d54fdb8d7db9e364015950
masxkip <jesufemiketiku@gmail.com>
2025-05-16 10:53:50 -0700

    new back deploy

--- index.js
+++ index.js
@@ -53,4 +53,4 @@ app.get("/api/protected", verifyToken, (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
